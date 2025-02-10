387ec0d9db111f3069c9ab374ffbf63a730a6fb8
VladislavGolovin96 <vsgolovin96@gmail.com>
2025-02-10 10:54:50 +0300

    added hw_11_2

---
+++ homework_11_2/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a[] = new int[5];
+        a[0] = 7; // 0 - ключ, 7 - значение
+
+        Pair<String, Integer> pair = new Pair<>("Марсель", 185);
+    }
+}
